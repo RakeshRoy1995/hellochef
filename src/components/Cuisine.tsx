2cba1bddd10890b56db0b3fe8b33e955df254585
rajibshovon <57267847+rajibshovon@users.noreply.github.com>
2024-03-14 19:43:59 +0600

    design

--- src/components/Cuisine.tsx
+++ src/components/Cuisine.tsx
@@ -1,18 +1,23 @@
 import Slider from "react-slick";
-import { slick_multiple_breakdown_settings, slick_multiple_breakdown_settings_cuisin } from "../utils/utils";
+import {
+  slick_multiple_breakdown_settings,
+  slick_multiple_breakdown_settings_cuisin,
+} from "../utils/utils";
 import { useSelector } from "react-redux";
 
 export default function Cuisine() {
   const category_data: any = useSelector((state: any) => state?.place);
-  
+
   return (
     <>
-      <div className="row controller-wrap">
-        <div className="col-md-6">
-          <h3>What's on your mind?</h3>
-        </div>
-        <div className="col-md-6">
-          {/* <div className="controller">
+      <section className="mind-wrapper">
+        <div className="container custom-max-width">
+          <div className="row controller-wrap">
+            <div className="col-md-6">
+              <h3>What's on your mind?</h3>
+            </div>
+            <div className="col-md-6">
+              {/* <div className="controller">
             <button className="prev prev1">
               <img src="image/left-icon.png" alt="" />
             </button>
@@ -20,38 +25,41 @@ export default function Cuisine() {
               <img src="image/right-icon.png" alt="" />
             </button>
           </div> */}
-        </div>
-      </div>
-
-      <div className="slider-container">
-        {category_data?.cuisines?.length > 0 && (
-          <div className="row">
-            <div className="col-md-12">
-              <Slider {...slick_multiple_breakdown_settings_cuisin}>
-                {category_data.cuisines.map((cat_data: any, key: any) => (
-                  <div className="item" key={key}>
-                    <div className="recipe cuisin-img">
-                      <img
-                      
-                        src={
-                          category_data?.get_default_config?.base_urls
-                            ?.cuisine_image_url +
-                          "/" +
-                          cat_data?.cuisine?.image
-                        }
-                        width={144}
-                        height={180}
-                        alt=""
-                      />
-                      <p style={{marginLeft:"30px"}}>{cat_data?.cuisine?.name}</p>
-                    </div>
-                  </div>
-                ))}
-              </Slider>
             </div>
           </div>
-        )}
-      </div>
+
+          <div className="slider-container">
+            {category_data?.cuisines?.length > 0 && (
+              <div className="row">
+                <div className="col-md-12">
+                  <Slider {...slick_multiple_breakdown_settings_cuisin}>
+                    {category_data.cuisines.map((cat_data: any, key: any) => (
+                      <div className="item" key={key}>
+                        <div className="recipe cuisin-img">
+                          <img
+                            src={
+                              category_data?.get_default_config?.base_urls
+                                ?.cuisine_image_url +
+                              "/" +
+                              cat_data?.cuisine?.image
+                            }
+                            width={144}
+                            height={180}
+                            alt=""
+                          />
+                          <p style={{ marginLeft: "30px" }}>
+                            {cat_data?.cuisine?.name}
+                          </p>
+                        </div>
+                      </div>
+                    ))}
+                  </Slider>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </section>
     </>
   );
 }
