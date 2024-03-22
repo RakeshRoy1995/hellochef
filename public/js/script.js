d74fb558051d997e773633233e2ecd92dc830aac
rajibshovon <57267847+rajibshovon@users.noreply.github.com>
2024-03-22 20:35:01 +0600

    menu items add

--- public/js/script.js
+++ public/js/script.js
@@ -83,6 +83,24 @@ $('#kolkata').owlCarousel({
 })
 
 
+$('#featured').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+
   // Reference to the Owl Carousel instance
   var owl = $('#mind');
   var owl2 = $('#kolkata');
